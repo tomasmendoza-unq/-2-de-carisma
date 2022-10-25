1a84a7b7bac044ef5fac47b77ef05a53c97a4c05
leopoldo109 <tm1453766@gmail.com>
2022-10-25 19:10:32 -0300

    se inicio el repo

--- public/src/js/main.js
+++ public/src/js/main.js
@@ -2,13 +2,22 @@ const link = 'https://api.open5e.com/classes'
 
 console.log("hola")
 
-function fetchClases(){
+// fetchweapons
+
+fetch("https://api.open5e.com/weapons")
+    .then((res) => res.json())
+    .then((data) => {
+        console.log(data)
+    });
+
+// function fetchClases(){
     fetch(link)
         .then((res) => res.json())
         .then((data) => {
+            console.log(data)
             document.querySelector(".spinner").classList.add("d-none")
         })
-}
+// }
 
 
 function dibujarCard(){
